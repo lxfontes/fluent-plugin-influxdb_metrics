f657e39abf6dd7660c1308031008ba7197ea36d5
lxfontes <lxfontes@gmail.com>
2014-05-25 10:40:37 -0400

    first commit

---
+++ fluent-plugin-influxdb-metrics.gemspec
@@ -0,0 +1,22 @@
+$:.push File.expand_path("../lib", __FILE__)
+
+Gem::Specification.new do |s|
+  s.name          = "fluent-plugin-influxdb-metrics"
+  s.version       = '0.0.1'
+  s.authors       = ["lxfontes"]
+  s.email         = ["lxfontes+influx@gmail.com"]
+  s.description   = %q{InfluxDB output plugin for Fluentd}
+  s.summary       = %q{output plugin for fluentd}
+  s.homepage      = "https://github.com/lxfontes/fluent-plugin-influxdb-metrics"
+  s.license       = 'MIT'
+
+  s.files         = `git ls-files`.split($/)
+  s.executables   = s.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
+  s.test_files    = s.files.grep(%r{^(test|spec|features)/})
+  s.require_paths = ["lib"]
+
+  s.add_runtime_dependency "fluentd"
+
+  s.add_development_dependency "rake"
+  s.add_development_dependency "webmock"
+end
---
+++ lib/fluent/plugin/out_influxdb_metrics.rb
@@ -0,0 +1,83 @@
+# encoding: utf-8
+require 'date'
+require 'net/http'
+
+class Fluent::InfluxdbOutput < Fluent::BufferedOutput
+  Fluent::Plugin.register_output('influxdb-metrics', self)
+
+  config_param :host, :string,  :default => 'localhost'
+  config_param :port, :integer,  :default => 8086
+  config_param :user, :string,  :default => 'root'
+  config_param :password, :string,  :default => 'root'
+  config_param :dbname, :string,  :default => 'fluentd'
+  config_param :table, :string, :default => 'metrics'
+  config_param :fields, :string, :default => ''
+
+  def initialize
+    super
+  end
+
+  def configure(conf)
+    super
+  end
+
+  def start
+    super
+  end
+
+  def filter_keys
+    @filter_keys ||= @fields.split(',').map(&:strip)
+    @filter_keys
+  end
+
+  def format(tag, time, record)
+    [tag, time, record].to_msgpack
+  end
+
+  def shutdown
+    super
+  end
+
+  def write(chunk)
+    bulk = []
+
+    chunk.msgpack_each do |tag, time, record|
+      formatted_record = format_record(tag, time, record)
+      bulk << formatted_record if formatted_record
+    end
+
+    http = Net::HTTP.new(@host, @port.to_i)
+    request = Net::HTTP::Post.new("/db/#{@dbname}/series?u=#{@user}&p=#{password}",
+                                  'Content-Type' => 'application/json; charset=utf-8')
+    request.body = Yajl::Encoder.encode(bulk)
+    http.request(request).value
+  end
+
+  def format_record(tag, time, record)
+    cols = ['time']
+    points = [time]
+
+    filter_keys.each do |field|
+      path = field.split('.')
+      rec_pos = record[path.shift]
+      path.each do |p|
+        rec_pos = rec_pos[p] if rec_pos
+      end
+
+      if rec_pos
+        cols << field
+        points << rec_pos
+      end
+    end
+
+    if cols.length > 1
+      return {
+        name: @table,
+        columns: cols,
+        points: points
+      }
+    end
+
+    nil
+  end
+end
